7ffe98680210064ed1fb9a681d5d60a9a6f39e81
darwin-s-walters <darwin.s.walters@gmail.com>
2017-02-12 14:16:23 -0500

    Added printing to console, still have to save to file

--- LiftCycle.py
+++ LiftCycle.py
@@ -1,28 +1,33 @@
-class LiftCycle:
+class LiftCycle(object):
 
 
-    lift_name
+    lift_name = ""
 
-    training_max
+    training_max = 0
 
-    warmup_sets[]
-    week1_sets[]
-    week2_sets[]
-    week3_sets[]
+    warmup_sets = []
+    week1_sets = []
+    week2_sets = []
+    week3_sets = []
 
     warmup_percentages = [0.4, 0.5, 0.6]
     week1_percentages = [0.65, 0.75, 0.85]
     week2_percentages = [0.7, 0.8, 0.9]
     week3_percentages = [0.75, 0.85, 0.95]
 
-    def __init__(self, lift_name, training_max):
-        lift_name = lift_name
-        traininng_max = training_max
+    def __init__( self, lift_name, training_max):
+        self.lift_name = lift_name
+        self.training_max = float(training_max)
 
-        warmup_sets = calculate_set_weights(training_max, warmup_percentages)
-        week1_sets = calculate_set_weights(training_max, week1_percentages)
-        week2_sets = calculate_set_weights(training_max, week2_percentages)
-        week3_sets = calculate_set_weights(training_max, week3_percentages)
+        self.warmup_sets = self.calculate_set_weights(self.training_max, LiftCycle.warmup_percentages)
+        self.week1_sets = self.calculate_set_weights(self.training_max, LiftCycle.week1_percentages)
+        self.week2_sets = self.calculate_set_weights(self.training_max, LiftCycle.week2_percentages)
+        self.week3_sets = self.calculate_set_weights(self.training_max, LiftCycle.week3_percentages)
 
+    def calculate_set_weights( self, training_max, percentages_list):
+        set_weight_list = []
+        for percentage_iterator in percentages_list:
+               set_weight_list.append( training_max * percentage_iterator )
 
-    def calculate_set_weights( traiing_max, percentages_list):
+        return set_weight_list 
+        
--- lift_calculator.py
+++ lift_calculator.py
@@ -1,5 +1,46 @@
 import LiftCycle
+import datetime
+
+def print_set_weights( set_weight_list ):
+    for current_set_weight in set_weight_list:
+        print current_set_weight
+
+
+def print_531_cycle( lift_cycle ):
+
+    print "\nLift: " + lift_cycle.lift_name
+    print "\n\nTraining Max: " + str(lift_cycle.training_max)
+    
+    print "\nWarmup Sets ( 5 reps each)"
+    print_set_weights(lift_cycle.warmup_sets)
+
+    print "\nWeek 1 Sets (5 reps each)"
+    print_set_weights(lift_cycle.week1_sets)
+
+    print "\nWeek 2 Sets (3 reps each)"
+    print_set_weights(lift_cycle.week2_sets)
+
+    print "\nWeek 3 Sets (5 , 3 , 1)"
+    print_set_weights(lift_cycle.week3_sets)
+    print "\n"
+
+def save_cycle_to_file( lift_cycle ):
+   current_time = datetime.datetime.now() 
+
+   timestamp = str(current_time.month) + "_" + str(current_time.day) + "_" + str(current_time.year)
+   file_name = lift_cycle.lift_name + "_" + timestamp 
+   
+   log_file = open( file_name, 'w' )
+   print >>log_file, print_531_cycle
+   # log_file.write( print_531_cycle( lift_cycle ) )
+   log_file.close()
+
+   
 
 training_max = raw_input("Please enter your training max: ")
+lift_name = raw_input("Please enter lift name: ")
 
-def calculate_lifts( training_max ):
+lift_cycle = LiftCycle.LiftCycle( lift_name, training_max )
+
+print_531_cycle( lift_cycle )
+# save_cycle_to_file( lift_cycle )
